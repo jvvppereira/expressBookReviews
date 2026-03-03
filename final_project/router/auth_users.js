7305bf8271d1fef09b1b06a2a3e2c26e5c8ed3ff
João Vítor <joao.vitor.paes@hotmail.com>
2026-03-03 16:01:23 -0300

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
+const books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
@@ -39,14 +39,50 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn
+    const book = books[isbn]
+
+    if (!book) {
+        return res.status(404).send(`Not found a book with ${isbn} ISBN`);
+    }
+
+    const newReview = req.body.review;
+    const nextIndex = Object.keys(book.reviews).length + 1
+
+    book.reviews[nextIndex] = { text: newReview, createdBy: req.user }
+
+    return res.status(200).send(`Review '${newReview}' was added to ${isbn} - '${book.title}' book`);
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
 });
 
 module.exports.authenticated = regd_users;
